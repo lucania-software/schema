6ea01424e4d36b884eafe809cb5db7f6e22499a4
Jeremy Bankes <jeremy.bankes@gmail.com>
2025-03-28 12:16:38 -0300

    Added EnumerationSchemaAny

--- source/typing/extended.ts
+++ source/typing/extended.ts
@@ -1,6 +1,8 @@
 import { BaseSchema } from "../schema/BaseSchema";
+import { EnumerationSchema } from "../schema/EnumerationSchema";
 import { ObjectSchema } from "../schema/ObjectSchema";
 import { DefaultValue } from "./toolbox";
 
 export type BaseSchemaAny = BaseSchema<any, any, boolean, DefaultValue<any>>;
 export type ObjectSchemaAny = ObjectSchema<any, boolean, DefaultValue<any>>;
+export type EnumerationSchemaAny = EnumerationSchema<string, boolean, DefaultValue<any>>;
